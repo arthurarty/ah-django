cdd5fa362a4a37f42374621e5431fbde4e787d51
arthurarty <arthur.nangai@gmail.com>
2018-11-21 22:39:16 +0300

    [ch] Add token routes for jwt token.

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url
-
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import (
     LoginAPIView, RegistrationAPIView, UserRetrieveUpdateAPIView
 )
 
 urlpatterns = [
     url(r'^user/?$', UserRetrieveUpdateAPIView.as_view()),
-    url(r'^users/?$', RegistrationAPIView.as_view()),
-    url(r'^users/login/?$', LoginAPIView.as_view()),
+    url(r'^users/?$', RegistrationAPIView.as_view(), name='authentication'),
+    url(r'^users/login/?$', LoginAPIView.as_view(), name='login'),
+    path('token', TokenObtainPairView.as_view()), 
+    path('token/refresh', TokenRefreshView.as_view()),
 ]
